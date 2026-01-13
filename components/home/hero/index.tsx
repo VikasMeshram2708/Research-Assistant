87fce8abd4093d91c80b3fff429aa008fa124673
VikasMeshram2708 <meshramvikas744@gmail.com>
2026-01-13 23:50:00 +0530

    add: workflows and testing.

---
+++ components/home/hero/index.tsx
@@ -0,0 +1,10 @@
+import { Button } from "../../ui/button";
+
+export default function Hero() {
+  return (
+    <div data-testId="hero">
+      <h1>Hero</h1>
+      <Button data-testid="get_started_btn">Get Started</Button>
+    </div>
+  );
+}
